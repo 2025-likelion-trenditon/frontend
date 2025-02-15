afd066d04ad11246b42f3967429fb458f2292fd3
jinhyo <kjhyo0220@gmail.com>
2025-02-16 03:19:31 +0900

    Fix: 순위별로 이미지 다르게 뜨도록 수정

--- src/components/Rank/RankCard.jsx
+++ src/components/Rank/RankCard.jsx
@@ -4,29 +4,31 @@ import rankIcon from "../../assets/img/rank_rankIcon.svg";
 import { useState } from "react";
 
 const RankCard = () => {
-  const [rankImg, setRankImg] = useState(rankTop3Icon);
-
   return (
     <>
-      {RANK.result.data.map((item) => (
-        <div className="Rank_card">
-          {/* 순위 */}
-          <div>
-            <img src={rankImg} alt="tkwlsdjqtdma" />
-          </div>
+      {RANK.result.data.map((item) => {
+        const rankImg = item.rank <= 3 ? rankTop3Icon : rankIcon;
+        return (
+          <div className="Rank_card" key={item.rank}>
+            {/* 순위 */}
+            <div className="Rank_img">
+              <img src={rankImg} />
+              <p>{item.rank}</p>
+            </div>
 
-          {/* 사진 및 닉네임 */}
-          <div className="Info_wrap">
-            {item.image ? <img src={item.image} /> : <div className="Img_box"></div>}
-            <p>{item.nickname}</p>
-          </div>
+            {/* 사진 및 닉네임 */}
+            <div className="Info_wrap">
+              {item.image ? <img src={item.image} /> : <div className="Img_box"></div>}
+              <p>{item.nickname}</p>
+            </div>
 
-          {/* 점수 */}
-          <div className="Score_wrap">
-            <p>{item.score.toLocaleString()}점</p>
+            {/* 점수 */}
+            <div className="Score_wrap">
+              <p>{item.score.toLocaleString()}점</p>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
